2aded29665e572c16381b605e86af5a0acc0a68a
Cuong Nguyen <cuongvnus2001@gmail.com>
2024-02-11 22:51:10 -0500

    completed assignment requirement

--- src/main/java/edu/scranton/cs/se584/wordscore/ComputeWordScore.java
+++ src/main/java/edu/scranton/cs/se584/wordscore/ComputeWordScore.java
@@ -1,18 +1,22 @@
 package edu.scranton.cs.se584.wordscore;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.map.InverseMapper;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.mapreduce.lib.reduce.LongSumReducer;
@@ -48,41 +52,37 @@ import org.json.simple.parser.ParseException;
  * output directory that contains each word and its score in descending order
  * by score. Words with equal score may appear in any order.
  * <p>
- * TODO(student) Document any improvements that you have made to the algorithm.
  *
  * @author Daniel M. Jackowitz
  * @author Cuong D. Nguyen
  */
 public class ComputeWordScore extends Configured implements Tool {
-  // You don't need to worry yet about Configured and Tool. We'll discuss them
-  // in lecture, but just know that they add some extra CLI parsing logic for
-  // framework-level configuration and then eventually call run(String[]) with
-  // the remaining, non-framework arguments left for your code to handle.
-  // For now, just think of run(String[]) as the standard Java main(String[]).
-  // TODO(student) Remove the preceding comment prior to submission.
 
   /**
-   * Produces (.stars, 1), where ".stars" is the value of the JSON field named "stars".
+   * Produces (.word, .score_modifier), where:
+   *  ".word" is a unique word from the collection of all text reviews.
+   *  ".score_modifier" is the modifier value corresponding to the star value of
+   *    the review in which the text appears.
    */
   public static class ComputeWordScoreMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
 
-    // For constant values you can create and share a single static Writable.
-    private static final LongWritable PLUS_TWO = new LongWritable(2);
-    private static final LongWritable PLUS_ONE = new LongWritable(1);
-    private static final LongWritable ZERO = new LongWritable(0);    
-    private static final LongWritable MINUS_ONE = new LongWritable(-1);
-    private static final LongWritable MINUS_TWO = new LongWritable(-2);
+    // Lookup table for score modifiers according to the star value. Shared across calls to map().
+    private static final Map<Integer, LongWritable> SCORE_MODIFIERS = new HashMap<>();
+    static {  // Ensure that this code block initializes HashMap, and only execute once.
+      SCORE_MODIFIERS.put(5, new LongWritable(2));
+      SCORE_MODIFIERS.put(4, new LongWritable(1));
+      SCORE_MODIFIERS.put(3, new LongWritable(0));
+      SCORE_MODIFIERS.put(2, new LongWritable(-1));
+      SCORE_MODIFIERS.put(1, new LongWritable(-2));
+    }
 
-    // Writables in Hadoop can and should be reused across calls to map().
-    // Non-constant Writables must not be shared across Mapper instances.
-    // private final IntWritable stars = new IntWritable();
+    // Non-constant Writable. Must not be shared across Mapper instances.
     private final Text word = new Text();
 
     // Each Mapper in MapReduce is only ever called from a single thread.
     private final JSONParser parser = new JSONParser();
 
     // Helper to encapsulate exception conversion and casting.
-    // This is just regular Java JSON parsing, nothing MapReduce-specific.
     private JSONObject parse(final String line) {
       try {
         return (JSONObject) parser.parse(line);
@@ -94,24 +94,13 @@ public class ComputeWordScore extends Configured implements Tool {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
       final JSONObject review = parse(value.toString());
-      // Update the value of the Writable and write (.stars, 1) to the Context.
-      int scoreModifier = ((Number) review.get("stars")).intValue();
+      final LongWritable scoreModifier = SCORE_MODIFIERS.get(((Number) review.get("stars")).intValue());
 
-      // References: WordCount.java Hadoop Example
+      // Iterator Reference: WordCount.java Hadoop Example
       StringTokenizer itr = new StringTokenizer(review.get("text").toString());
       while (itr.hasMoreTokens()) {
         word.set(itr.nextToken());
-        if (scoreModifier==5) {
-          context.write(word, PLUS_TWO);
-        } else if (scoreModifier==4) {
-          context.write(word, PLUS_ONE);
-        } else if (scoreModifier == 3) {
-          context.write(word, ZERO);
-        } else if (scoreModifier == 2) {
-          context.write(word, MINUS_ONE);
-        } else if (scoreModifier == 1) {
-          context.write(word, MINUS_TWO);
-        }
+        context.write(word, scoreModifier);
       }
     }
   }
@@ -122,50 +111,62 @@ public class ComputeWordScore extends Configured implements Tool {
       System.err.println("Usage: <input> <output>");
       return 2; // Return 2 to signal a problem with the arguments.
     }
-
-    // This is the second part of the Configured/Tool "magic" mentioned above.
-    // You're definitely going to need a Configuration object and you'll want
-    // to use the one from getConf() rather than creating your own in order to
-    // benefit from the functionality added by Configured/Tool.
-    // TODO(student) Remove the preceding comment prior to submission.
+    
     final Configuration conf = getConf();
-    final Job job = Job.getInstance(conf, "Compute Word Score");
+    final Path inputPath = new Path(arguments[0]);
+    final Path outputPath = new Path(arguments[1]);
+    final Path intermediatePath = new Path(outputPath, "intermediate");
+    final Path finalPath = new Path(outputPath, "final");
 
-    // Hadoop needs to know which JAR file contains the job code.
-    // This JAR will be uploaded to the cluster when the job is submitted.
-    job.setJarByClass(ComputeWordScore.class);
+    final Job scorer = Job.getInstance(conf, "Compute Word Score");
+    scorer.setJarByClass(ComputeWordScore.class);
 
-    // We're using files for both input and output, taken from the arguments.
-    job.setInputFormatClass(TextInputFormat.class); // This is the default.
-    FileInputFormat.addInputPath(job, new Path(arguments[0]));
-    job.setOutputValueClass(TextOutputFormat.class); // This is the default.
-    FileOutputFormat.setOutputPath(job, new Path(arguments[1]));
+    scorer.setInputFormatClass(TextInputFormat.class);
+    FileInputFormat.addInputPath(scorer, inputPath);
+    /*
+     * On SequenceFile(I/O)Format, DMJ,
+     * "This format is particularly useful for passing data between multiple jobs…".
+     * It eliminated the need to parse this intermediate file in the 2nd job.
+     */
+    scorer.setOutputFormatClass(SequenceFileOutputFormat.class);
+    FileOutputFormat.setOutputPath(scorer, intermediatePath);
 
-    // Register the Mapper and Reducer implementations with the Hadoop
-    // framework. Note that unlike in our SimpleMapReduce these are not
-    // instances, but the Class objects. This is because Hadoop needs not
-    // just a single instance of each, but the ability to construct as many
-    // instances of Mappers and Reducers as it requires, one for each task.
-    job.setMapperClass(ComputeWordScoreMapper.class);
-    job.setReducerClass(LongSumReducer.class);
+    scorer.setMapperClass(ComputeWordScoreMapper.class);
+    scorer.setReducerClass(LongSumReducer.class);
+    // Adding a combiner GREATLY reduces I/O access
+    scorer.setCombinerClass(LongSumReducer.class);
 
-    // These are required so Hadoop knows how to (de)serialize the output.
-    // If it seems redundant, research "type erasure" with Java generics.
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(LongWritable.class);
+    scorer.setOutputKeyClass(Text.class);
+    scorer.setOutputValueClass(LongWritable.class);
 
-    // Finally, submit the job and wait for it to complete. Everything
-    // prior was just setup, this actually triggers real computation.
-    // When using ToolRunner it's expected to return an integer value
-    // to be used as the process return code (i.e. 0 for success).
-    return job.waitForCompletion(true) ? 0 : 1;
+
+    final Job sorter = Job.getInstance(conf, "Sort Word Score");
+    sorter.setJarByClass(ComputeWordScore.class);
+
+    sorter.setInputFormatClass(SequenceFileInputFormat.class);
+    FileInputFormat.addInputPath(sorter, intermediatePath);
+    sorter.setOutputFormatClass(TextOutputFormat.class);
+    FileOutputFormat.setOutputPath(sorter, finalPath);
+
+    /*
+     * Initially, I wrote my own mapper to swap and sort the scores, but I
+     * found out that Hadoop has built-in tools to achieve the same result.
+     * 
+     * A quick lookup of "descending" and "decreasing" in the Hadoop source code
+     * reveals the DecreasingComparator class
+     * 
+     * InverseMapper is one of the Direct Known Subclasses of Mapper in the docs
+     */
+    sorter.setSortComparatorClass(LongWritable.DecreasingComparator.class);
+    sorter.setMapperClass(InverseMapper.class);
+
+    sorter.setOutputKeyClass(LongWritable.class);
+    sorter.setOutputValueClass(Text.class);
+
+    return (scorer.waitForCompletion(true) && sorter.waitForCompletion(true)) ? 0 : 1;
   }
 
   public static void main(String[] arguments) throws Exception {
-    // ToolRunner is a common Hadoop utility that wraps argument parsing with
-    // some helpers to populate a Configuration. You can then later access this
-    // populated Configuration using getConf().
-    // TODO(student) Remove the preceding comment prior to submission.
     System.exit(ToolRunner.run(new ComputeWordScore(), arguments));
   }
 }
